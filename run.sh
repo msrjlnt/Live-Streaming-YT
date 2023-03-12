0a171169dddc9b4a3f0925874180cf581c6b1950
msrjlnt <114513169+msrjlnt@users.noreply.github.com>
2023-03-12 16:03:36 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,25 +1,35 @@
-#!/bin/bash
+#! /bin/bash
+#
+# Diffusion youtube avec ffmpeg
 
-# Konfigurasi youtube dengan resolusi 720p. Video tidak di-scaled.
-VBR="2500k"                                    
-FPS="30"                                       
-QUAL="medium"                                  
-YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"  
+# Configurer youtube avec une résolution 720p. La vidéo n'est pas scalée.
 
-# Masukkan path file video yang ingin di-loop
-SOURCE="video.mp4" 
+VBR="2500k"                                    # Bitrate de la vidéo en sortie
+FPS="30"                                       # FPS de la vidéo en sortie
+QUAL="medium"                                  # Preset de qualité FFMPEG
+YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"  # URL de base RTMP youtube
 
-# Masukkan kunci yang diberikan oleh youtube pada event streaming
-KEY="<your key>"   
+FOLDER="videos"                                    # Dossier source
+KEY="PUT YOUR KEY THERE"                                     # Clé à récupérer sur l'event youtube
 
-# Loop ffmpeg secara terus-menerus menggunakan perintah while dan sleep
-while true; do
-    ffmpeg \
-        -i "$SOURCE" -deinterlace \
-        -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
-        -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
-        -f flv "$YOUTUBE_URL/$KEY" \
+SOURCE=""
+n=0
+filter=""
 
-    # Tidur selama 2 detik sebelum melakukan iterasi berikutnya
-    sleep 2
+for f in $FOLDER/*.mp4
+do
+  SOURCE="$SOURCE -i $f"
+  filter="$filter [$n:v:0] [$n:a:0]"
+  ((n++))
 done
+
+filter="$filter concat=n=$n:v=1:a=1 [v] [a]"
+
+echo "ffmpeg $SOURCE -filter_complex '$filter'"
+
+ffmpeg \
+    $SOURCE -filter_complex "$filter" \
+-map "[v]" -map "[a]" -deinterlace \
+    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
+    -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
+    -f flv "$YOUTUBE_URL/$KEY"
